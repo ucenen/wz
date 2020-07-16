cc5836a278893bd06aee735ad06aa6cb8b985dd2
ucen <1779926694@qq.com>
2020-07-16 18:26:21 +0800

    first commit

---
+++ www.yunjiglobal.com的副本/res/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+	var hm = document.createElement("script");
+	hm.src = "https://hm.baidu.com/hm.js?5ff9c5c0f67520d73f71409ed616bfc8";
+	var s = document.getElementsByTagName("script")[0];
+	s.parentNode.insertBefore(hm, s);
+})();
